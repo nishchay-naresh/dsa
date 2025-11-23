cc6106fdd2ffbba1d94dd5d15fc9a6054c4b790a
Nishchay Naresh <nishchay.naresh@gmail.com>
2025-11-23 14:22:00 +0530

    + linklist.a02easy.ReverseAList.java

---
+++ src/main/java/com/nishchay/ds/linklist/a02easy/MiddleNode.java
@@ -0,0 +1,58 @@
+package com.nishchay.ds.linklist.a02easy;
+
+
+/*
+ *  ======================= Middle Node in a Linked List ====================
+ *
+ *  Given the head of singly linked list, find middle node of the linked list.
+ *      If the number of nodes is odd, return the middle node.
+ *      If the number of nodes is even, there are two middle nodes, so return the second middle node.
+ *
+ * https://www.geeksforgeeks.org/dsa/write-a-c-function-to-print-the-middle-of-the-linked-list/
+ *
+ * */
+
+import com.nishchay.ds.linklist.Node;
+import com.nishchay.ds.linklist.Utils;
+
+import static com.nishchay.ds.linklist.Utils.createList;
+import static com.nishchay.ds.linklist.Utils.printList;
+
+public class MiddleNode {
+
+    public static void main(String[] args) {
+        // odd list
+        Node head = Utils.createList();
+        printList(head);
+        System.out.println("The middle element is = " + getMiddleNode(head));
+
+        // even list
+        head = createList(new int[]{10, 20, 30, 40, 50, 60});
+        printList(head);
+        System.out.println("The middle element is = " + getMiddleNode(head));
+    }
+
+    /*
+     *  ================ [Optimize/Expected Approach] Hare and Tortoise Algorithm - O(n) Time and O(1) Space =====================
+     *
+     * We can use the Tortoise and Hare algorithm to find the middle of the linked list.
+     *      Initialize both slow and fast pointers at the head.
+     *      Move slow by one step and fast by two steps each iteration.
+     *      When fast reaches the end (or null), slow will be at the middle.
+     *      For even nodes, slow automatically ends at the second middle.
+     *
+     * Time Complexity  : O(n)
+     * Space Complexity : O(1)
+     */
+    private static int getMiddleNode(Node head) {
+        Node slowPointer, fastPointer;
+
+        slowPointer = fastPointer = head;
+        while (fastPointer != null && fastPointer.next != null) {
+            fastPointer = fastPointer.next.next;
+            slowPointer = slowPointer.next;
+        }
+        assert slowPointer != null;
+        return slowPointer.data;
+    }
+}
---
+++ src/main/java/com/nishchay/ds/linklist/a02easy/ReverseAList.java
@@ -0,0 +1,53 @@
+package com.nishchay.ds.linklist.a02easy;
+
+/*
+ *  ======================= Reverse a Linked List ====================
+ *
+ *  Given the head of a linked list, reverse the list and return the new head.
+ *     Input    : head -> 1 -> 2 -> 3 -> 4 -> 5 -> null
+ *     Output   : head -> 5 -> 4 -> 3 -> 2 -> 1 -> null
+ *
+ *
+ * https://www.geeksforgeeks.org/dsa/reverse-a-linked-list/
+ *
+ * */
+
+import com.nishchay.ds.linklist.Node;
+import com.nishchay.ds.linklist.Utils;
+
+import static com.nishchay.ds.linklist.Utils.printList;
+
+public class ReverseAList {
+
+    public static void main(String[] args) {
+        Node head = Utils.createList();
+        printList(head);
+        System.out.println("--------- Reversed list -----------");
+        head = reverse(head);
+        printList(head);
+    }
+
+    /*
+     *  ================ [Optimize/Expected Approach] Using Iterative Method - O(n) Time and O(1) Space =====================
+     *
+     * The idea is to reverse the linked list by changing the direction of links using three pointers: prev, curr, and next.
+     *     At each step, point the current node to its previous node and then move all three pointers forward until the list is fully reversed.
+     *
+     * Time Complexity  : O(n)
+     * Space Complexity : O(1)
+     */
+    public static Node reverse(Node head) {
+        Node prev = null;
+        Node current = head;
+        Node next;
+
+        while (current != null) {
+            next = current.next;
+            current.next = prev;
+            prev = current;
+            current = next;
+        }
+        head = prev;
+        return head;
+    }
+}
